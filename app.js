9f049a07b68889c83693a4c059d06b1bc10ba36f
aadesh1214 <60508886+aadesh1214@users.noreply.github.com>
2020-09-14 21:51:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//SELECTORS
+const todoInput = document.querySelector(".tsk1");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(event) {
+  event.preventDefault();
+//Todo div
+const todoDiv = document.createElement("div");
+todoDiv.classList.add("Todo");
+//create LI
+const newTodo = document.createElement("li");
+newTodo.innerText = todoInput.value;
+newTodo.classList.add("todo=item");
+todoDiv.appendChild(newTodo);
+//check mark button
+const completedButton = document.createElement("button");
+completedButton.innerHTML = '<i class="fas fa-check"></i>';
+completedButton.classList.add("complete-btn");
+todoDiv.appendChild(completedButton);
+//Trash button
+const trashButton = document.createElement("button");
+trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+trashButton.classList.add("trash-btn");
+todoDiv.appendChild(trashButton);
+//Append to List
+todoList.appendChild(todoDiv);
+//clear todo input value
+todoInput.value = "";
+}
